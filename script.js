346e7b41fb8f6790c9fb6a4ab7e358d5d1b478ef
AimarMayo <aimarmayokorta@gmail.com>
2026-04-22 08:45:56 +0200

    Web orrialdea
    Dokumentuak igotzerakoan arazo bat izan dugu eta dokumentu guztiak ezabatu zaizkigu, berri igo ditugu gauzak

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  emailjs.init("b3sZSjYEyGojEY7B8");
+
+  const formularioa = document.getElementById("formularioa");
+  const erantzuna = document.getElementById("erantzuna");
+
+  if (formularioa) {
+    formularioa.addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      emailjs.sendForm("service_co875ff", "template_gdxths9", this)
+        .then(function (response) {
+          console.log("OK:", response);
+          erantzuna.textContent = "Mezua bidali da!";
+          erantzuna.classList.remove("text-danger");
+          erantzuna.classList.add("text-success");
+          formularioa.reset();
+        })
+        .catch(function (error) {
+          console.error("EMAILJS ERROR:", error);
+          erantzuna.textContent = "Errorea gertatu da.";
+          erantzuna.classList.remove("text-success");
+          erantzuna.classList.add("text-danger");
+        });
+    });
+  }
+});
